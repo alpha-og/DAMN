2d63cd9b1306cb510204a958991c575b282e64f7
Athul Anoop <athulanoop2020@gmail.com>
2023-08-01 20:27:36 +0530

    test commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Router basename="/DAMN/">
+        <Router>
             <App />
         </Router>
     </React.StrictMode>
