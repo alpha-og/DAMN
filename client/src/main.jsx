d290b4d30f25848a4ac96e307770d3c3782b8ea2
Athul Anoop <athulanoop2020@gmail.com>
2023-08-01 20:26:23 +0530

    Added tailwindcss to the project and changed some configurations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// react imports
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+// file imports
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Router basename="/DAMN/">
+            <App />
+        </Router>
+    </React.StrictMode>
+);
