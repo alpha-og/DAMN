99d7a6f6f76356dda41960ca142103559a18c5ba
Athul Anoop <athulanoop2020@gmail.com>
2023-08-02 11:19:43 +0530

    Fixed a minor bug with the navrail
    Fixed a bug that caused the navrail to render inconsistently across different browsers. The solution was a simple restructure of the JSX that made the navbar and navrail components

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
                 <ul className="md:flex hidden flex-row ml-auto ">
                     {routes.map((route, index) => (
                         <li
-                            className="flex flex-row ml-2 rounded-lg hover:cursor-pointer hover:text-violet-600 hover:bg-slate-900 hover:shadow-sm hover:shadow-violet-500 ease-in-out duration-500"
+                            className="flex flex-row ml-2 rounded-lg hover:cursor-pointer hover:text-violet-600 hover:bg-slate-900 hover:shadow-sm hover:shadow-violet-500 hover:scale-105 ease-in-out duration-500"
                             key={index}
                         >
                             <Link className="px-7 py-1" to={route.path}>
@@ -28,41 +28,41 @@ const Navbar = () => {
                 </ul>
                 <AiOutlineMenu
                     size={28}
-                    className="md:hidden block p-1 ml-auto hover:cursor-pointer hover:text-violet-600 ease-in-out duration-500 ease-in-out duration-500"
+                    className="md:hidden block p-1 ml-auto hover:cursor-pointer hover:text-violet-600 ease-in-out duration-500"
                     onClick={toggleNavrail}
                 />
-                <div
-                    className={
-                        navrail
-                            ? "fixed top-5 left-5 w-1/2 h-[95%] p-5 rounded-xl bg-slate-900 bg-opacity-60 backdrop-blur-md ease-in-out duration-500"
-                            : "fixed top-5 -left-full w-1/2 h-[95%] p-5 rounded-xl bg-slate-900 bg-opacity-60 backdrop-blur-md ease-in-out duration-500"
-                    }
-                >
-                    <div className="flex flex-row items-center pl-5">
-                        <Branding />
-                        <AiOutlineClose
-                            size={28}
-                            className="p-1 ml-auto hover:text-violet-600 ease-in-out duration-500"
-                            onClick={toggleNavrail}
-                        />
-                    </div>
-
-                    <ul className="mt-10 flex flex-col">
-                        {routes.map((route, index) => (
-                            <li
-                                className="flex flex-row w-full mb-5 rounded-lg hover:cursor-pointer hover:text-violet-600 hover:bg-slate-900 hover:shadow-sm hover:shadow-violet-500 ease-in-out duration-500"
-                                key={index}
-                            >
-                                <Link
-                                    className=" w-full px-5 py-2.5"
-                                    to={route.path}
-                                >
-                                    {route.title}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
+            </nav>
+            <nav
+                className={
+                    navrail
+                        ? "fixed top-5 left-5 w-1/2 h-[95%] p-5 rounded-xl bg-slate-900 bg-opacity-60 backdrop-blur-md ease-in-out duration-500"
+                        : "fixed top-5 -left-full w-1/2 h-[95%] p-5 rounded-xl bg-slate-900 bg-opacity-60 backdrop-blur-md ease-in-out duration-500"
+                }
+            >
+                <div className="flex flex-row items-center pl-5">
+                    <Branding />
+                    <AiOutlineClose
+                        size={28}
+                        className="p-1 ml-auto hover:text-violet-600 hover:cursor-pointer ease-in-out duration-500"
+                        onClick={toggleNavrail}
+                    />
                 </div>
+
+                <ul className="mt-10 flex flex-col">
+                    {routes.map((route, index) => (
+                        <li
+                            className="flex flex-row w-full mb-5 rounded-lg hover:cursor-pointer hover:text-violet-600 hover:bg-slate-900 hover:shadow-sm hover:shadow-violet-500 hover:scale-105 ease-in-out duration-500"
+                            key={index}
+                        >
+                            <Link
+                                className=" w-full px-5 py-2.5"
+                                to={route.path}
+                            >
+                                {route.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             </nav>
         </div>
     );
